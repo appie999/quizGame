a09116037197cc1871fe965562ac3ce4ef431219
appie999 <aitoutmghartfarid@gmail.com>
2025-04-17 10:21:23 +0100

    add api

---
+++ src/app/services/trivia.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TriviaService {
+  private apiUrl = 'https://opentdb.com/api.php';
+  constructor(private http: HttpClient) { }
+
+  // @ts-ignore
+  getQuestions(amount: number = 10 , category?: number, difficulty?: string, type: string = 'multiple'): Observable<any>{
+    let url =`${this.apiUrl}?amount=${amount}&type=${type}`;
+    if (category) url +=`&category=${category}`;
+    if(difficulty) url +=`&difficulty=${difficulty}`;
+    return this.http.get<any>(url);
+  }
+}
